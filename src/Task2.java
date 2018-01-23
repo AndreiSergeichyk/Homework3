500414c19776e27559d1dcaeee977041d1d8fd84
AndreiSergeichyk <35502287+AndreiSergeichyk@users.noreply.github.com>
2018-01-23 10:58:14 +0300

    Update Task2.java
    n--;

--- src/Task2.java
+++ src/Task2.java
@@ -5,7 +5,7 @@ public class Task2 {
     public static void main(String[] args) {
 
         int n = new Scanner(System.in).nextInt();
-
+        n--;
         while (n>=0){
             System.out.println(fibonachi(n));
             n--;
